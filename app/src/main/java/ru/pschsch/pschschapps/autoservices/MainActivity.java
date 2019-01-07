d840f1030f8c6e2e6a0a89f53891c8c685f596b4
Daniil Kovalev <x1opya@gmail.com>
2019-01-07 23:54:26 +0500

    another test commit

--- app/src/main/java/ru/pschsch/pschschapps/autoservices/MainActivity.java
+++ app/src/main/java/ru/pschsch/pschschapps/autoservices/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int x1opyaBrachInit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
